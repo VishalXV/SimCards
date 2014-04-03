3628e2c8fcb2d6d0fd8640490a21d2a148803953
nini1294 <nishant301294@gmail.com>
2014-04-03 18:01:13 -0400

    options menu

---
+++ src/com/example/views/HostActivity.java
@@ -0,0 +1,34 @@
+package com.example.views;
+
+import com.example.simcards.R;
+import com.example.simcards.R.layout;
+import com.example.simcards.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class HostActivity extends AbstractMVPBluetoothActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_host);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.host, menu);
+		return true;
+	}
+
+	@Override
+	public void onDeviceResponse(boolean success) {
+		
+		Log.i("BT","Device Responce");
+		
+	}
+
+}
